c20a63de838220be29e0b1f75da9ce347aeb2646
Tu Nombre <you@example.com>
2021-06-15 15:55:48 +0200

    doc fuera

---
+++ src/main/java/curso/cap/springboot/aspectos/AvisadorDeAudiencia.java
@@ -0,0 +1,15 @@
+package curso.cap.springboot.aspectos;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class AvisadorDeAudiencia {
+	
+	
+	@Pointcut("")
+
+}
